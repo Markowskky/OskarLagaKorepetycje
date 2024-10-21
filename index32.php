b5196c915bd18acdd3ab00bf49cbdfb7d35410a9
MarekPo <131866939+Markowskky@users.noreply.github.com>
2024-10-21 09:25:51 +0200

    Add files via upload

--- index32.php
+++ index32.php
@@ -7,27 +7,40 @@
 <body>
 <?php 
 
-$con = mysqli_connect("localhost","root","","poniedzialek");
- if(mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " .mysqli_connect_error();
-    exit();
-    }else{
-        echo "Connection ok";
-    }
-    if(!empty($_POST['imie']) && !empty($_POST['nazwisko']) && !empty($_POST['wiek'])){
-    $addsql = "INSERT INTO users ( id, imie, nazwisko, wiek) VALUES (NULL, "" . $_POST['imie'] . "," . $_POST['nazwisko'] . "," . $_POST['wiek']
-    "}
+$con = mysqli_connect("localhost", "root", "", "poniedzialek");
+ if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   exit();
+} else {
+    echo "Connection ok<br>";
+    $addsql = "INSERT INTO users ( id, imie, nazwisko, wiek) VALUES (NULL, 'Szymon', 'Sprzedawczyk' , 60)";
+ mysqli_query($con,$addsql);
+}
+if(!empty($_POST['imie']) && !empty($_POST['nazwisko']) && !empty($_POST['wiek'])){
+    //$addsql = "INSERT INTO users ( id, imie, nazwisko, wiek) VALUES (NULL, '" . $_POST['imie'] . "', '" . $_POST['nazwisko'] . "', '". $_POST['wiek']. ")";
+  }
 
-    if(mysqli_query($con_$addsql)){
-        echo "RECORD ADDED";
-    }else{
-        echo "ADD ERROR";
-    }
+    echo "<prep>";
+    print_r($_POST);
+    echo "</prep>";
+  
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($con,$sql);
 
-
- 
- ?>
-
-
+        echo'<table border="1">';
+        while($row = mysqli_fetch_assoc($result)){
+            //mysqli_fetch_row
+            //mysqli_fetch_array
+            //mysqli_fetch_assoc
+            echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
+            echo "<td>" . $row['imie'] . "</td>";
+            echo "<td>" . $row['nazwisko'] . "</td>";
+            echo "<td>" . $row['wiek'] . "</td>";
+            echo "</tr>";
+        }
+            echo "</table>"
+        
+    ?>
 </body>
 </html>
